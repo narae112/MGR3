7c29d6e6acadaa3d952d8c0a51bdfa5296bad853
narae <narae0967@gmail.com>
2024-05-18 11:39:51 +0900

    커밋테스트

---
+++ src/main/java/com/MGR/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.MGR.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity //스프링 시큐리티 핉터가 필터 체인에 등록
+public class SecurityConfig extends WebSecurityConfiguration {
+//protected 로 메서드 설정 주의
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.formLogin((login) -> login
+                        .usernameParameter("email")
+                        .failureUrl("/member/login/error")
+                        .loginPage("/member/login")
+                        .defaultSuccessUrl("/"))
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)); // 세션 삭제
+
+        return http.build();
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
