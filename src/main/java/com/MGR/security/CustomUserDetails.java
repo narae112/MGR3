fbe491a467b9d1da4a9ffcef636f5397ac0c2391
narae112 <narae0967@gmail.com>
2024-05-21 12:14:19 +0900

    카카오 api 수정중

--- src/main/java/com/MGR/security/CustomUserDetails.java
+++ src/main/java/com/MGR/security/CustomUserDetails.java
@@ -3,13 +3,16 @@ package com.MGR.security;
 import com.MGR.entity.Member;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
-public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails implements UserDetails, OAuth2User {
 
     private final Member member;
+    private Map<String,Object> attributes;
 
     public CustomUserDetails(Member member){
         this.member = member;
@@ -48,5 +51,20 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public CustomUserDetails(Member member, Map<String, Object> attributes){
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
--- src/main/java/com/MGR/security/SecurityConfig.java
+++ src/main/java/com/MGR/security/SecurityConfig.java
@@ -28,33 +28,39 @@ import java.util.List;
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 //        http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
         // csrf 토큰 나중에 다시 생성
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        http.formLogin((login) -> login
-                    .loginPage("/member/login")
-                    .usernameParameter("email")
-                    .passwordParameter("password")
-                    .failureUrl("/member/login/error")
-                    .defaultSuccessUrl("/")
-                    .loginProcessingUrl("/member/login"))
-            .logout((logout) -> logout
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                    .logoutSuccessUrl("/")
-                    .invalidateHttpSession(true)); // 세션 삭제
-
-        http.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated());
-
-        http.userDetailsService(customUserDetailsService);
+                        .anyRequest().permitAll()
+                )
+                .formLogin(login -> login
+                        .loginPage("/member/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .failureUrl("/member/login/error")
+                        .defaultSuccessUrl("/")
+                        .loginProcessingUrl("/member/login")
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .loginPage("/member/kakao/login")
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(customOAuth2UserService)
+                        )
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                )
+                .userDetailsService(customUserDetailsService);
 
         return http.build();
     }
@@ -63,11 +69,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public CustomAuthenticationProvider customAuthenticationProvider() {
-//        return new CustomAuthenticationProvider(memberRepository, passwordEncoder);
-//    }
 
     @Bean
     public CommandLineRunner initDb(MemberRepository memberRepository){
@@ -88,4 +89,24 @@ public class SecurityConfig {
             }
         };
     }
+
+//    @Bean
+//    public CommandLineRunner initDbUser(MemberRepository memberRepository){
+//
+//        return createAdmin -> {
+//            boolean isAdminPresent = memberRepository.findByName("사용자").isPresent();
+//
+//            if (!isAdminPresent) {
+//                Member user = new Member();
+//
+//                user.setName("사용자");
+//                user.setEmail("mrg2024@kakao.com");
+//                user.setNickname("초기사용자");
+//                user.setPassword(passwordEncoder().encode("USER"));
+//                user.setRole(Role.USER);
+//
+//                memberRepository.save(user);
+//            }
+//        };
+//    }
 }
