1f348a4fbb25a99aa9e4273cb4fea5b5d61d77f8
hahahahadd <161662107+hahahahadd@users.noreply.github.com>
2024-05-22 12:45:45 +0900

    티켓등록

--- src/main/java/com/MGR/entity/Ticket.java
+++ src/main/java/com/MGR/entity/Ticket.java
@@ -55,19 +55,7 @@ public class Ticket extends BaseEntity {
 
     }
 
-    @PrePersist
-    public void checkDuplicateName() {
-        // 티켓 이름이 이미 존재하는지 확인하고 예외를 발생시킴
-        if (nameAlreadyExists(name)) {
-            throw new DuplicateTicketNameException("티켓명 '" + name + "'은(는) 이미 존재합니다.");
-        }
-    }
 
-    private boolean nameAlreadyExists(String name) {
-        // 여기에서는 티켓 이름이 이미 데이터베이스에 존재하는지 확인하는 로직을 구현
-        // 데이터베이스에서 해당 이름을 가진 티켓을 조회하여 존재 여부를 판단할 수 있음
-        return false; // 구현에 따라서 true 또는 false 반환
-    }
 
 }
 
