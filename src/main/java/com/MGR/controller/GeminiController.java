ba049208e2da0e247c73f2e11b3056d86407c4df
narae112 <narae0967@gmail.com>
2024-05-29 17:33:49 +0900

    gemini - 주석처리

--- src/main/java/com/MGR/controller/GeminiController.java
+++ src/main/java/com/MGR/controller/GeminiController.java
@@ -1,29 +1,29 @@
-package com.MGR.controller;
-
-import com.MGR.service.GeminiService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/gemini")
-public class GeminiController {
-
-    private final GeminiService geminiService;
-
-    @GetMapping("/chat")
-    public ResponseEntity<?> gemini(@RequestParam String message) {
-        try {
-            String response = geminiService.getContents(message);
-            return ResponseEntity.ok().body(response);
-        } catch (HttpClientErrorException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-}
+//package com.MGR.controller;
+//
+//import com.MGR.service.GeminiService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.HttpClientErrorException;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/gemini")
+//public class GeminiController {
+//
+//    private final GeminiService geminiService;
+//
+//    @GetMapping("/chat")
+//    public ResponseEntity<?> gemini(@RequestParam String message) {
+//        try {
+//            String response = geminiService.getContents(message);
+//            return ResponseEntity.ok().body(response);
+//        } catch (HttpClientErrorException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
+//}
--- src/main/java/com/MGR/dto/ChatRequestDto.java
+++ src/main/java/com/MGR/dto/ChatRequestDto.java
@@ -1,49 +1,49 @@
-package com.MGR.dto;
-
-import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class ChatRequestDto { //챗봇한테 요청하는 dto
-    private List<Content> contents; //채팅 요청의 내용을 담고 있는 리스트
-    private GenerationConfig generationConfig; //텍스트 생성 설정을 담고 있는 객체
-
-    @Getter @Setter
-    public static class Content {
-        private Parts parts; //api 한테 메세지 보낼때 json{content{parts}} 이런 형식
-    }
-
-    @Getter @Setter
-    public static class Parts {
-        private String text; //
-
-    }
-
-    @Getter @Setter
-    public static class GenerationConfig {
-        private int candidate_count; //생성할 텍스트 수
-        private int max_output_tokens; //생성할 텍스트의 최대 토큰 수
-        private double temperature; // 텍스트 생성의 다양성 조절
-
-    }
-
-    public ChatRequestDto(String prompt) { //설정 초기화
-        this.contents = new ArrayList<>();
-        Content content = new Content();
-        Parts parts = new Parts();
-
-        parts.setText(prompt);
-        content.setParts(parts);
-
-        this.contents.add(content);
-        this.generationConfig = new GenerationConfig();
-        this.generationConfig.setCandidate_count(1);
-        this.generationConfig.setMax_output_tokens(1000);
-        this.generationConfig.setTemperature(0.7);
-    }
-}
+//package com.MGR.dto;
+//
+//import lombok.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class ChatRequestDto { //챗봇한테 요청하는 dto
+//    private List<Content> contents; //채팅 요청의 내용을 담고 있는 리스트
+//    private GenerationConfig generationConfig; //텍스트 생성 설정을 담고 있는 객체
+//
+//    @Getter @Setter
+//    public static class Content {
+//        private Parts parts; //api 한테 메세지 보낼때 json{content{parts}} 이런 형식
+//    }
+//
+//    @Getter @Setter
+//    public static class Parts {
+//        private String text; //
+//
+//    }
+//
+//    @Getter @Setter
+//    public static class GenerationConfig {
+//        private int candidate_count; //생성할 텍스트 수
+//        private int max_output_tokens; //생성할 텍스트의 최대 토큰 수
+//        private double temperature; // 텍스트 생성의 다양성 조절
+//
+//    }
+//
+//    public ChatRequestDto(String prompt) { //설정 초기화
+//        this.contents = new ArrayList<>();
+//        Content content = new Content();
+//        Parts parts = new Parts();
+//
+//        parts.setText(prompt);
+//        content.setParts(parts);
+//
+//        this.contents.add(content);
+//        this.generationConfig = new GenerationConfig();
+//        this.generationConfig.setCandidate_count(1);
+//        this.generationConfig.setMax_output_tokens(1000);
+//        this.generationConfig.setTemperature(0.7);
+//    }
+//}
