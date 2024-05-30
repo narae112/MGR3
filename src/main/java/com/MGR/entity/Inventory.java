95175c40753e8a32b4d73935e1509724a01b89b2
hyewon8558 <hw8558@gmail.com>
2024-05-30 16:27:16 +0900

    재고 수량 매일 자정마다 100개로 지정

--- src/main/java/com/MGR/entity/Inventory.java
+++ src/main/java/com/MGR/entity/Inventory.java
@@ -3,14 +3,15 @@ package com.MGR.entity;
 import com.MGR.exception.OutOfStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
-@Getter @Setter
-@RequiredArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class Inventory {
 
     @Id
@@ -23,6 +24,14 @@ public class Inventory {
 
     private int quantity; // 재고 수량
     private LocalDate date; // 날짜
+
+    public static Inventory createInventory(Ticket ticket, int quantity, LocalDate date) {
+        Inventory inventory = new Inventory();
+        inventory.setTicket(ticket);
+        inventory.setQuantity(quantity);
+        inventory.setDate(date);
+        return inventory;
+    }
 
     public void removeQuantity(int reserveCount) {
         int rest = this.quantity - reserveCount; // 남은 재고 = 현재 재고 - 넘어온 주문수량
