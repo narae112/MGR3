5cdf5d2b68afd283716f50ee05be6ec5d77b390f
youngin0902 <161662107+youngin0902@users.noreply.github.com>
2024-05-31 13:09:55 +0900

    Update and rename QnaQuestionRepository.java to ReviewBoardRepository.java

--- src/main/java/com/MGR/repository/ReviewBoardRepository.java
+++ src/main/java/com/MGR/repository/ReviewBoardRepository.java
@@ -1,6 +1,6 @@
 package com.MGR.repository;
 
-import com.MGR.entity.QnaQuestion;
+import com.MGR.entity.ReviewBoard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -10,17 +10,17 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface QnaQuestionRepository extends JpaRepository<QnaQuestion, Long> {
-    QnaQuestion findBySubject(String subject);
-    QnaQuestion findBySubjectAndContent(String subject, String content);
-    List<QnaQuestion> findBySubjectLike(String subject);
-    Page<QnaQuestion> findAll(Pageable pageable);
-    Page<QnaQuestion> findAll(Specification<QnaQuestion> spec, Pageable pageable);
+public interface ReviewBoardRepository extends JpaRepository<ReviewBoard, Long> {
+    ReviewBoard findBySubject(String subject);
+    ReviewBoard findBySubjectAndContent(String subject, String content);
+    List<ReviewBoard> findBySubjectLike(String subject);
+    Page<ReviewBoard> findAll(Pageable pageable);
+    Page<ReviewBoard> findAll(Specification<ReviewBoard> spec, Pageable pageable);
 
     @Query("select distinct q "
-            + "from QnaQuestion q "
+            + "from ReviewBoard q "
             + "left join q.author u1 "
-            + "left join q.answerList a "
+            + "left join q.commentList a "
             + "left join a.author u2 "
             + "where "
             + "   q.subject like %:kw% "
@@ -28,6 +28,6 @@ public interface QnaQuestionRepository extends JpaRepository<QnaQuestion, Long> 
             + "   or u1.nickname like %:kw% "
             + "   or a.content like %:kw% "
             + "   or u2.nickname like %:kw% ")
-    Page<QnaQuestion> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
+    Page<ReviewBoard> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 
 }
