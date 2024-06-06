cd1a020ebacaeeebb9b1cc088cf338f8fc557315
hyewon8558 <hw8558@gmail.com>
2024-06-06 15:37:35 +0900

    240606 1537 hyewon

--- src/main/java/com/MGR/entity/OrderTicket.java
+++ src/main/java/com/MGR/entity/OrderTicket.java
@@ -35,25 +35,58 @@ public class OrderTicket {
     private int childCount; // 아동 주문 수량
     private LocalDate visitDate;
 
+//    public static OrderTicket createOrderTicket(Ticket ticket, ReservationTicket reservationTicket, int adultCount, int childCount, LocalDate visitDate) {
+//
+//        OrderTicket orderTicket = new OrderTicket();
+//
+//        orderTicket.setTicket(ticket);
+//        orderTicket.setReservationTicket(reservationTicket);
+//        orderTicket.setAdultPrice(ticket.getAdultPrice());
+//        orderTicket.setChildCount(ticket.getChildPrice());
+//        orderTicket.setAdultCount(adultCount);
+//        orderTicket.setChildCount(childCount);
+//        orderTicket.setVisitDate(visitDate);
+//
+//        return orderTicket;
+//    } // 주문할 상품과 주문 수량을 통해 orderTicket 객체를 만드는 메서드 작성
+//
+//    public int getAdultTotalPrice() { return adultPrice * adultCount; } // 성인 티켓 토탈 가격(성인 티켓 가격 * 성인 인원수)
+//    public int getChildTotalPrice() {
+//        return childPrice * childCount;
+//    } // 아동 티켓 토탈 가격
+//
+//    public int getTotalPrice() { return ((adultPrice * adultCount) + (childPrice * childCount)); } // 전체(성인 + 아동) 주문 가격
+public OrderTicket() {
+}
+
+    // 생성자 추가
+    public OrderTicket(Ticket ticket, ReservationTicket reservationTicket, int adultCount, int childCount, LocalDate visitDate) {
+        this.ticket = ticket;
+        this.reservationTicket = reservationTicket;
+        this.adultPrice = ticket.getAdultPrice();
+        this.childPrice = ticket.getChildPrice();
+        this.adultCount = adultCount;
+        this.childCount = childCount;
+        this.visitDate = visitDate;
+    }
+
+    // 주문할 상품과 주문 수량을 통해 orderTicket 객체를 만드는 메서드 작성
     public static OrderTicket createOrderTicket(Ticket ticket, ReservationTicket reservationTicket, int adultCount, int childCount, LocalDate visitDate) {
+        return new OrderTicket(ticket, reservationTicket, adultCount, childCount, visitDate);
+    }
 
-        OrderTicket orderTicket = new OrderTicket();
+    // 성인 티켓 토탈 가격(성인 티켓 가격 * 성인 인원수)
+    public int getAdultTotalPrice() {
+        return adultPrice * adultCount;
+    }
 
-        orderTicket.setTicket(ticket);
-        orderTicket.setReservationTicket(reservationTicket);
-        orderTicket.setAdultPrice(ticket.getAdultPrice());
-        orderTicket.setChildCount(ticket.getChildPrice());
-        orderTicket.setAdultCount(adultCount);
-        orderTicket.setChildCount(childCount);
-        orderTicket.setVisitDate(visitDate);
-
-        return orderTicket;
-    } // 주문할 상품과 주문 수량을 통해 orderTicket 객체를 만드는 메서드 작성
-
-    public int getAdultTotalPrice() { return adultPrice * adultCount; } // 성인 티켓 토탈 가격(성인 티켓 가격 * 성인 인원수)
+    // 아동 티켓 토탈 가격
     public int getChildTotalPrice() {
         return childPrice * childCount;
-    } // 아동 티켓 토탈 가격
+    }
 
-    public int getTotalPrice() { return (getAdultTotalPrice() + getChildTotalPrice()); } // 전체(성인 + 아동) 주문 가격
+    // 전체(성인 + 아동) 주문 가격
+    public int getTotalPrice() {
+        return getAdultTotalPrice() + getChildTotalPrice();
+    }
 }
