f400ae14ad4b4b60c07f6f181e4a33f4242e47d8
narae112 <narae0967@gmail.com>
2024-06-06 15:38:00 +0900

    시큐리티 수정중 0606 1537

--- src/main/java/com/MGR/security/CustomUserDetailsService.java
+++ src/main/java/com/MGR/security/CustomUserDetailsService.java
@@ -1,30 +1,30 @@
-package com.MGR.security;
-
-import com.MGR.entity.Member;
-import com.MGR.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final MemberRepository memberRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        Optional<Member> memberOptional = memberRepository.findByEmail(email);
-        Member member = memberOptional.orElseThrow(() -> new UsernameNotFoundException(email));
-
-//        return new CustomUserDetails(member);
-        return new PrincipalDetails(member);
-    }
-}
+//package com.MGR.security;
+//
+//import com.MGR.entity.Member;
+//import com.MGR.repository.MemberRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//@Transactional
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    private final MemberRepository memberRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//
+//        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+//        Member member = memberOptional.orElseThrow(() -> new UsernameNotFoundException(email));
+//
+////        return new CustomUserDetails(member);
+//        return new PrincipalDetails(member);
+//    }
+//}
