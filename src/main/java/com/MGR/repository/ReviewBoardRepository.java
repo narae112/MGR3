b480a1c1ea00bdeecbb34ec3c2772e3ce4cbedbc
youngin0902 <161662107+youngin0902@users.noreply.github.com>
2024-06-07 15:01:05 +0900

    Update ReviewBoardRepository.java

--- src/main/java/com/MGR/repository/ReviewBoardRepository.java
+++ src/main/java/com/MGR/repository/ReviewBoardRepository.java
@@ -45,17 +45,17 @@ public interface ReviewBoardRepository extends JpaRepository<ReviewBoard, Long> 
             "order by q.count desc")
     Page<ReviewBoard> findByKeywordOrderByCountDesc(@Param("kw") String kw, Pageable pageable);
 
-    @Query("select distinct q from ReviewBoard q " +
-            "left join q.author u1 " +
-            "left join q.commentList a " +
-            "left join a.author u2 " +
-            "left join q.voter v " +
-            "where q.subject like %:kw% " +
-            "or q.content like %:kw% " +
-            "or u1.nickname like %:kw% " +
-            "or a.content like %:kw% " +
-            "or u2.nickname like %:kw% " +
-            "group by q " +
-            "order by count(v) desc")
+    @Query("SELECT q FROM ReviewBoard q " +
+            "LEFT JOIN q.author u1 " +
+            "LEFT JOIN q.commentList a " +
+            "LEFT JOIN a.author u2 " +
+            "LEFT JOIN q.voter v " +
+            "WHERE q.subject LIKE %:kw% " +
+            "OR q.content LIKE %:kw% " +
+            "OR u1.nickname LIKE %:kw% " +
+            "OR a.content LIKE %:kw% " +
+            "OR u2.nickname LIKE %:kw% " +
+            "GROUP BY q.id " +
+            "ORDER BY COUNT(v) DESC")
     Page<ReviewBoard> findByKeywordOrderByVoterCountDesc(@Param("kw") String kw, Pageable pageable);
 }
