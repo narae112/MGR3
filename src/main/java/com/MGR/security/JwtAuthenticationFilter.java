2ad3852b9e857f882787131081346d849e36ca48
narae <narae0967@gmail.com>
2024-06-11 20:27:42 +0900

    쿠키 인증 시간 늘림

--- src/main/java/com/MGR/security/JwtAuthenticationFilter.java
+++ src/main/java/com/MGR/security/JwtAuthenticationFilter.java
@@ -87,7 +87,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         accessTokenCookie.setSecure(true); // HTTPS 사용 시에만 설정
         accessTokenCookie.setPath("/");
 //        accessTokenCookie.setMaxAge(5 * 60); //시간 안맞아서 일단 수정
-        accessTokenCookie.setMaxAge(24 * 60 * 60);
+        accessTokenCookie.setMaxAge(7 * 24 * 60 * 60);
         response.addCookie(accessTokenCookie);
 
         // Refresh Token 생성 및 쿠키에 설정
@@ -96,7 +96,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         refreshTokenCookie.setHttpOnly(true);
         refreshTokenCookie.setSecure(true); // HTTPS 사용 시에만 설정
         refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setMaxAge(24 * 60 * 60); // 24시간 동안 유효
+        refreshTokenCookie.setMaxAge(7 * 24 * 60 * 60); // 24시간 동안 유효
         response.addCookie(refreshTokenCookie);
 
         // Redis 에 RefreshToken 저장
