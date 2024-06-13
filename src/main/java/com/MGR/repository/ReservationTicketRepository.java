da4b8cb06b6819cdac632f07a52d071dbe1e52e1
narae112 <narae0967@gmail.com>
2024-06-13 17:18:09 +0900

    예약내역 정렬순서, 페이징

--- src/main/java/com/MGR/repository/ReservationTicketRepository.java
+++ src/main/java/com/MGR/repository/ReservationTicketRepository.java
@@ -3,6 +3,7 @@ package com.MGR.repository;
 import com.MGR.dto.ReservationDtlDto;
 import com.MGR.entity.Reservation;
 import com.MGR.entity.ReservationTicket;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,16 +17,16 @@ public interface ReservationTicketRepository extends JpaRepository<ReservationTi
     List<ReservationTicket> findByReservationIdAndTicketId(Long reservationId, Long ticketId);
     ReservationTicket findByTicketId(Long ticketId);
     List<ReservationTicket> findAllByTicketId(Long ticketId);
+
     @Query("select rt from ReservationTicket rt " +
             "where rt.reservation.id = :reservationId " +
-            "and rt.reservationStatus = 'RESERVE' " +
-            "order by rt.reservationDate desc"
-    )
-    List<ReservationDtlDto> findReservations(@Param("reservationId")Long id, Pageable pageable);
+            "and (rt.reservationStatus = 'RESERVE' or rt.reservationStatus = 'CANCEL') " +
+            "order by rt.reservationStatus desc, rt.reservationDate desc")
+    Page<ReservationTicket> findByReservationId(@Param("reservationId") Long reservationId, Pageable pageable);
 
     @Query("select count(rt) from ReservationTicket rt " +
-            "where rt.reservation.id = :reservationId "
-    )
-    Long countReservation(@Param("reservationId")Long id);
+            "where rt.reservation.id = :reservationId " +
+            "and (rt.reservationStatus = 'RESERVE' or rt.reservationStatus = 'CANCEL')")
+    Long countReservation(@Param("reservationId") Long reservationId);
 
 }
