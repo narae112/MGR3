9cb5fe3fed6ee984f1acb6c8234f79921cd06796
hahahahadd <161662107+hahahahadd@users.noreply.github.com>
2024-06-13 23:51:58 +0900

    뷰 추천수반영 / 하트 아직 구현 x...

--- src/main/java/com/MGR/controller/ReviewBoardController.java
+++ src/main/java/com/MGR/controller/ReviewBoardController.java
@@ -36,6 +36,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -257,7 +258,7 @@ public class ReviewBoardController {
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/vote/{id}")
     public String reviewVote(@AuthenticationPrincipal PrincipalDetails member,
-                             @PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+                             @PathVariable("id") Long id, RedirectAttributes redirectAttributes,Model model) {
         if (member == null) {
             redirectAttributes.addFlashAttribute("error", "로그인이 필요한 서비스입니다.");
             return "member/loginForm";
@@ -266,10 +267,16 @@ public class ReviewBoardController {
         try {
             ReviewBoard reviewBoard = this.reviewBoardService.getReviewBoard(id);
             Member siteUser = this.memberService.getUser(member.getName());
-            if (reviewBoard.getVoter().contains(siteUser)) {
-                // 이미 투표한 경우에는 투표 취소
+            Set<Member> voters = reviewBoard.getVoter(); // 리뷰의 추천자 목록을 가져옵니다.
+            boolean isVoted = voters != null && voters.contains(siteUser);
+            // 현재 사용자가 리뷰를 추천했는지 여부를 확인합니다.
+            model.addAttribute("isVoted", isVoted);
+            // 추천 여부에 따라 동작을 수행합니다.
+            if (isVoted) {
+                // 이미 추천한 경우, 추천을 취소합니다.
                 this.reviewBoardService.cancelVote(reviewBoard, siteUser);
             } else {
+                // 추천하지 않은 경우, 추천을 합니다.
                 this.reviewBoardService.vote(reviewBoard, siteUser);
             }
         } catch (Exception e) {
