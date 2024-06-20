c0b11990a59ac918c90bfb9d564dbfe045bf1fc8
narae112 <narae0967@gmail.com>
2024-06-20 11:06:44 +0900

    레이아웃 탑버튼 제거, 메인 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -19,17 +19,18 @@ Author: GrayGrids
     /*=====================================
     Sticky
     ======================================= */
+document.addEventListener("DOMContentLoaded", function () {
     window.onscroll = function () {
-        var nav_logo = document.querySelector(".nav_logo");
-        var nav_logo_color = document.querySelector(".nav_logo_color");
-
-        if (window.pageYOffset === 0) { // 스크롤이 0일 때
-            nav_logo_color.style.display = "none";
-            nav_logo.style.display = "block";
-        } else {
-            nav_logo.style.display = "none";
-            nav_logo_color.style.display = "block";
-        }
+//        var nav_logo = document.querySelector(".nav_logo");
+//        var nav_logo_color = document.querySelector(".nav_logo_color");
+//
+//        if (window.pageYOffset === 0) { // 스크롤이 0일 때
+//            nav_logo_color.style.display = "none";
+//            nav_logo.style.display = "block";
+//        } else {
+//            nav_logo.style.display = "none";
+//            nav_logo_color.style.display = "block";
+//        }
 
         var header_navbar = document.querySelector(".navbar-area");
         var sticky = header_navbar.offsetTop;
@@ -50,6 +51,7 @@ Author: GrayGrids
         }
 
     };
+});
 
     // WOW active
     new WOW().init();
