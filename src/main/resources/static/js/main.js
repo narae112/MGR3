abe73fdc197c956737796a89da5ac36689371e00
sonhyewon <hw8558@gmail.com>
2024-06-21 04:05:13 +0900

    css 조금씩 수정함

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -21,16 +21,16 @@ Author: GrayGrids
     ======================================= */
 document.addEventListener("DOMContentLoaded", function () {
     window.onscroll = function () {
-//        var nav_logo = document.querySelector(".nav_logo");
-//        var nav_logo_color = document.querySelector(".nav_logo_color");
-//
-//        if (window.pageYOffset === 0) { // 스크롤이 0일 때
-//            nav_logo_color.style.display = "none";
-//            nav_logo.style.display = "block";
-//        } else {
-//            nav_logo.style.display = "none";
-//            nav_logo_color.style.display = "block";
-//        }
+        var nav_logo = document.querySelector(".nav_logo");
+        var nav_logo_color = document.querySelector(".nav_logo_color");
+
+        if (window.pageYOffset === 0) { // 스크롤이 0일 때
+            nav_logo_color.style.display = "none";
+            nav_logo.style.display = "block";
+        } else {
+            nav_logo.style.display = "none";
+            nav_logo_color.style.display = "block";
+        }
 
         var header_navbar = document.querySelector(".navbar-area.main");
         var sticky = header_navbar.offsetTop;
